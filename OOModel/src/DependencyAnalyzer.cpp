22a92525a71740b14f12b678b9cb192552ce1051
patlueth <patlueth@cab-e81-18.ethz.ch>
2015-10-19 15:36:45 +0200

    dev

--- OOModel/src/DependencyAnalyzer.cpp
+++ OOModel/src/DependencyAnalyzer.cpp
@@ -30,30 +30,100 @@
 
 namespace OOModel {
 
-void DependencyAnalyzer::handleStuff(Model::Node* node)
+void DependencyAnalyzer::associateWithFile(Model::Node* node, QString file, QHash<Model::Node*, QString>& nodeToFileMap)
 {
-	QVector<ReferenceExpression*> refs;
-	getRefs(node, &refs);
-	qDebug() << "inside depanalyzer" << node->typeName() << refs.size();
-	QSet<QString> nameSet;
-	for (auto r : refs)
+	nodeToFileMap.insert(node, file);
+}
+
+QString DependencyAnalyzer::file(Model::Node* node, QHash<Model::Node*, QString>& nodeToFileMap)
+{
+	if (!node) return "NOT_ASSOCIATED";
+
+	auto it = nodeToFileMap.find(node);
+	return it != nodeToFileMap.end() ? *it : file(node->parent(), nodeToFileMap);
+}
+
+void DependencyAnalyzer::associateNodesWithFiles(Model::Node* current, QString namespaceName,
+																 QHash<Model::Node*, QString>& nodeToFileMap)
+{
+	if (Module* ooModule = DCast<Module>(current))
 	{
-		if (auto t = r->target())
+		if (ooModule->classes()->size() > 0)
 		{
-			if (node->isAncestorOf(t))
-				continue;
+			namespaceName = ooModule->name();
 		}
-
-		auto d = getDependency(r);
-
-		if (d)
-			nameSet.insert(d->name());
 		else
-			nameSet.insert("unknown");
+		{
+			// macro file
+			associateWithFile(ooModule, namespaceName + "/" + ooModule->name(), nodeToFileMap);
+			return;
+		}
 	}
 
+	if (Class* ooClass = DCast<Class>(current))
+	{
+		associateWithFile(ooClass, namespaceName + "/" + ooClass->name(), nodeToFileMap);
+		return;
+	}
+
+	for (auto child : current->children())
+		associateNodesWithFiles(child, namespaceName, nodeToFileMap);
+}
+
+void DependencyAnalyzer::handleStuff(Model::Node* node)
+{
+	auto root = node;
+	while (root->parent()) root = root->parent();
+	QHash<Model::Node*, QString> nodeToFileMap;
+	associateNodesWithFiles(root, "", nodeToFileMap);
+
+	auto thisFile = file(node, nodeToFileMap);
+
+	QVector<ReferenceExpression*> refs;
+	getRefs(node, &refs);
+	qDebug() << "inside depanalyzer" << node->typeName() << "file:" << thisFile;
+
+	QSet<QString> nameSet;
+	for (ReferenceExpression* r : refs)
+	{
+		if (auto ooMethod = r->firstAncestorOfType<Method>())
+			if (ooMethod->items()->isAncestorOf(r))
+				continue;
+
+		if (auto t = r->target())
+		{
+			nameSet.insert((softDependency(r, nodeToFileMap) ? "soft " : "") + file(t, nodeToFileMap));
+		}
+	}
+
+	nameSet.remove("NOT_ASSOCIATED");
+	nameSet.remove(thisFile);
 	for (auto e : nameSet)
 		qDebug() << e;
+}
+
+bool DependencyAnalyzer::softDependency(OOModel::ReferenceExpression* reference,
+													 QHash<Model::Node*, QString>& nodeToFileMap)
+{
+	auto p = reference->parent();
+	Q_ASSERT(p);
+
+	if (reference->name() == "libraryRoot")
+	{
+		qDebug() << "soft dependency?" << reference->name();
+		qDebug() << reference->target() << (reference->target() ? reference->target()->typeName() : "");
+		qDebug() << (reference->target() ? file(reference->target(), nodeToFileMap) : "unknown");
+		qDebug() << reference->firstAncestorOfType<MethodCallExpression>();
+		qDebug() << DCast<PointerTypeExpression>(p) << DCast<ReferenceTypeExpression>(p);
+		qDebug() << p->typeName() << p->parent()->typeName();
+	}
+
+	if (reference->firstAncestorOfType<MethodCallExpression>()) return false;
+
+	if (DCast<TypeQualifierExpression>(p)) p = p->parent();
+	if (!DCast<PointerTypeExpression>(p) && !DCast<ReferenceTypeExpression>(p)) return false;
+
+	return true;
 }
 
 void DependencyAnalyzer::getRefs(Model::Node* node, QVector<ReferenceExpression*>* result)
